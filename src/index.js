212a258e1c2e1103c0e5648f07cc398f6a6f762f
bmgwaro <brian.gwaro@student.moringaschool.com>
2024-07-06 01:34:45 +0300

    fetch on demand

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 const init = () => {
-  
-}
+    document.addEventListener('DOMContentLoaded', init);
+    const inputForm = document.querySelector("form");
+    inputForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const input1=event.target.children[1].value;
 
-document.addEventListener('DOMContentLoaded', init);
+        fetch(`http://localhost:3000/movies/${input1}`)
+        .then((response) => response.json())
+        .then((data) => {
+            const title = document.querySelector("section#movieDetails h4");
+            const summary = document.querySelector("section#movieDetails p");
+
+            title.innerText = data.title;
+            summary.innerText = data.summary;
+        });
+      });
+}
+document.addEventListener("DOMContentLoaded", init);
